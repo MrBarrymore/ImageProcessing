a31ecf87c78fa73e4784518c1d7a9fa138c51afc
Misha Shcherbakov <shms.shcherbakov@gmail.com>
2019-03-12 14:40:13 +0700

    Добавьте файлы проекта.

---
+++ ImageProcessingLaba1/MainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ImageProcessingLaba1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.яркостьконтрастностьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.цветовойБалансToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.повыситьРезкостьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.размытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(568, 328);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1244, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(48, 20);
+            this.toolStripMenuItem1.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть...";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как...";
+            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.яркостьконтрастностьToolStripMenuItem,
+            this.цветовойБалансToolStripMenuItem,
+            this.повыситьРезкостьToolStripMenuItem,
+            this.размытьToolStripMenuItem});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(69, 20);
+            this.toolStripMenuItem2.Text = "Фильтры";
+            // 
+            // яркостьконтрастностьToolStripMenuItem
+            // 
+            this.яркостьконтрастностьToolStripMenuItem.Name = "яркостьконтрастностьToolStripMenuItem";
+            this.яркостьконтрастностьToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.яркостьконтрастностьToolStripMenuItem.Text = "Яркость/контрастность";
+            // 
+            // цветовойБалансToolStripMenuItem
+            // 
+            this.цветовойБалансToolStripMenuItem.Name = "цветовойБалансToolStripMenuItem";
+            this.цветовойБалансToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.цветовойБалансToolStripMenuItem.Text = "Цветовой баланс";
+            // 
+            // повыситьРезкостьToolStripMenuItem
+            // 
+            this.повыситьРезкостьToolStripMenuItem.Name = "повыситьРезкостьToolStripMenuItem";
+            this.повыситьРезкостьToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.повыситьРезкостьToolStripMenuItem.Text = "Повысить резкость";
+            // 
+            // размытьToolStripMenuItem
+            // 
+            this.размытьToolStripMenuItem.Name = "размытьToolStripMenuItem";
+            this.размытьToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.размытьToolStripMenuItem.Text = "Размыть";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(651, 36);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(568, 328);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 425);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "MainForm";
+            this.Text = "Обработка изображений";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem яркостьконтрастностьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem цветовойБалансToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem повыситьРезкостьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem размытьToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ ImageProcessingLaba1/MainForm.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ImageProcessingLaba1
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public static Bitmap image;
+        public static string full_name_of_image = "\0";
+
+
+        public struct RGB
+        {
+            public int R;
+            public int G;
+            public int B;
+        };
+
+        public RGB[,] BitmapToByteRgbNaive(Bitmap bmp)
+        {
+            int width = bmp.Width,
+                height = bmp.Height;
+            RGB[,] res = new RGB[height, width];
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    Color color = bmp.GetPixel(x, y);
+                    res[y, x].R = color.R;
+                    res[y, x].G = color.G;
+                    res[y, x].B = color.B;
+                }
+            }
+            return res;
+        }
+
+        public Bitmap RgbToBitmap(RGB [,] pixel)
+        {
+            Bitmap bmp;
+            int height = pixel.GetLength(1);
+            int width = pixel.GetLength(0);
+
+            RGB[,] res = new RGB[height, width];
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    pixel = image.GetPixel(x, y);
+                    bmp.SetPixel(x, y, pixel);
+                }
+            }
+            return bmp;
+        }
+
+
+        public void ByteWriteFile(RGB [,] pixel, int H, int W)
+        {
+            StreamWriter sw = new StreamWriter("pass.txt");
+            string s1 = " ";
+                for (int y = 0; y < H; y++)
+                {
+                    s1 = "";
+                    for (int x = 0; x < W; x++)
+                    {
+                        s1 += "(" + pixel[y, x].R + " ";
+                        s1 += pixel[y, x].G + " ";
+                        s1 += pixel[y, x].B + ") " + " ";
+                }
+                    s1 += "\n";
+                    sw.Write(s1);
+                }
+                s1 += "\n";      
+            sw.Close();
+        }
+
+        /*
+        public unsafe static byte[,,] BitmapToByteRgb(Bitmap bmp)
+        {
+            int width = bmp.Width,
+                height = bmp.Height;
+            byte[,,] res = new byte[3, height, width];
+
+            BitmapData bd = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+            try
+            {
+                byte* curpos;
+                for (int h = 0; h < height; h++)
+                {
+                    curpos = ((byte*)bd.Scan0) + h * bd.Stride;
+                    for (int w = 0; w < width; w++)
+                    {
+                        res[2, h, w] = *(curpos++);
+                        res[1, h, w] = *(curpos++);
+                        res[0, h, w] = *(curpos++);
+                    }
+                }
+            }
+            finally
+            {
+                bmp.UnlockBits(bd);
+            }
+            return res;
+        }
+        */
+
+        public RGB[,] ConvertToGray(RGB [,] pixel)
+        {
+
+
+
+            return pixel;
+        }
+
+
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    full_name_of_image = open_dialog.FileName;
+                    image = new Bitmap(open_dialog.FileName);
+
+                    RGB[,] pixel;
+
+                    this.pictureBox1.Size = image.Size;
+                    this.pictureBox2.Size = image.Size;
+
+                    this.Height = this.pictureBox2.Height + 75;
+
+
+                    //получение матрицы с пикселями
+                    pixel = new RGB[image.Height, image.Width];
+                    pixel = BitmapToByteRgbNaive(image);
+
+
+                    // Записываем рисунок в текстовый файл 
+                    // ByteWriteFile(pixel, Height, Width);
+
+
+                    pictureBox1.Image = image;
+                    // pictureBox1.Invalidate();
+
+
+                    pixel = ConvertToGray(pixel);
+
+
+                    for (int y = 0; y < Height; ++y)
+                    {
+                        for (int x = 0; x < Width; ++x)
+                        {
+                             image.SetPixel(x, y, pixel);
+                        }
+                    }
+
+                    pictureBox2.Image = image;
+                }
+                catch
+                {
+                    full_name_of_image = "\0";
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                //string format = full_name_of_image.Substring(full_name_of_image.Length - 4, 4);
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Сохранить картинку как...";
+                savedialog.OverwritePrompt = true;
+                savedialog.CheckPathExists = true;
+                savedialog.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+                savedialog.ShowHelp = true;
+                if (savedialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+            }
+        }
+    }
+}
